1a9f057d69e87c84b03398ab4911e159804d354b
MarcosEstPam <marcosepamias@gmail.com>
2024-08-26 23:41:33 +0000

    avances

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 import "../../styles/demo.css";
-import { isElementOfType } from "react-dom/test-utils";
 
 export const Demo = () => {
 	const { store, actions } = useContext(Context);
@@ -17,7 +16,6 @@ export const Demo = () => {
 
     const openModal = (id) => {
 		setShowModal(true);
-		console.log("se activa el proceso")
 		const foundContact = store.contacts.find((elemento) => elemento.id === id);
 		console.log (foundContact)
 		setName(foundContact.name)
@@ -46,6 +44,7 @@ export const Demo = () => {
     const closeModal = () => {
         setShowModal(false);
     };
+	const imgUrl = "https://fotografias.antena3.com/clipping/cmsimages01/2021/05/02/26E03450-C5FB-4D16-BC9B-B282AE784352/57.jpg?crop=1868,1080,x52,y0&width=1280&height=740&optimize=low&format=webply"
 
 	return (
 		<div className="container min-vh-100">
@@ -53,24 +52,39 @@ export const Demo = () => {
 				{store.contacts.map((contactos, index) => {
 					return (
 							<li key={index} className="list-group-item d-flex justify-content-between align-items-center">
-								<div className="d-flex flex-column mx-5">
-									<h6>{contactos.name}</h6>
-									<span><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-envelope-at" viewBox="0 0 16 16">
-									<path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2zm3.708 6.208L1 11.105V5.383zM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2z"/>
-									<path d="M14.247 14.269c1.01 0 1.587-.857 1.587-2.025v-.21C15.834 10.43 14.64 9 12.52 9h-.035C10.42 9 9 10.36 9 12.432v.214C9 14.82 10.438 16 12.358 16h.044c.594 0 1.018-.074 1.237-.175v-.73c-.245.11-.673.18-1.18.18h-.044c-1.334 0-2.571-.788-2.571-2.655v-.157c0-1.657 1.058-2.724 2.64-2.724h.04c1.535 0 2.484 1.05 2.484 2.326v.118c0 .975-.324 1.39-.639 1.39-.232 0-.41-.148-.41-.42v-2.19h-.906v.569h-.03c-.084-.298-.368-.63-.954-.63-.778 0-1.259.555-1.259 1.4v.528c0 .892.49 1.434 1.26 1.434.471 0 .896-.227 1.014-.643h.043c.118.42.617.648 1.12.648m-2.453-1.588v-.227c0-.546.227-.791.573-.791.297 0 .572.192.572.708v.367c0 .573-.253.744-.564.744-.354 0-.581-.215-.581-.8Z"/>
-									</svg> {contactos.email}</span>
-									<span> {contactos.phone}</span>
-									<span> {contactos.address}</span>
+								<div className="d-flex justify-content-between align-items-center">
+									<div className="d-flex align-items-center">
+										<img 
+											src={imgUrl} 
+											className="img-thumbnail" 
+											style={{ width: "200px", height: "200px", borderRadius: "50%", objectFit: "cover" }} 
+										/>
+										<div className="d-flex flex-column mx-4">
+											<h6>{contactos.name}</h6>
+											<span className="fw-light">
+												{contactos.email}
+											</span>
+											<span className="fw-light">
+												 {contactos.phone}
+											</span>
+											<span className="fw-light">
+												 {contactos.address}
+											</span>
+										</div>
+									</div>
+									<div>
+										<button className="btn text-success justify-content-end m-2" onClick={() => openModal(contactos.id)}>
+											<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-pen-fill" viewBox="0 0 16 16">
+												<path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001"/>
+											</svg>
+										</button>
+										<button className="btn text-danger justify-content-end " onClick={() => actions.deleteContact(contactos.id)}>
+											<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-trash2" viewBox="0 0 16 16">
+												<path d="M14 3a.7.7 0 0 1-.037.225l-1.684 10.104A2 2 0 0 1 10.305 15H5.694a2 2 0 0 1-1.973-1.671L2.037 3.225A.7.7 0 0 1 2 3c0-1.105 2.686-2 6-2s6 .895 6 2M3.215 4.207l1.493 8.957a1 1 0 0 0 .986.836h4.612a1 1 0 0 0 .986-.836l1.493-8.957C11.69 4.689 9.954 5 8 5s-3.69-.311-4.785-.793"/>
+											</svg>
+										</button>
+									</div>
 								</div>
-								<div>
-									<button className="btn text-success m-2" onClick={() => openModal(contactos.id)}><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-pen-fill" viewBox="0 0 16 16">
-  									<path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001"/>
-									</svg></button>
-									<button className="btn text-danger" onClick={() => actions.deleteContact(contactos.id)}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-trash2" viewBox="0 0 16 16">
-  									<path d="M14 3a.7.7 0 0 1-.037.225l-1.684 10.104A2 2 0 0 1 10.305 15H5.694a2 2 0 0 1-1.973-1.671L2.037 3.225A.7.7 0 0 1 2 3c0-1.105 2.686-2 6-2s6 .895 6 2M3.215 4.207l1.493 8.957a1 1 0 0 0 .986.836h4.612a1 1 0 0 0 .986-.836l1.493-8.957C11.69 4.689 9.954 5 8 5s-3.69-.311-4.785-.793"/>
-									</svg></button>
-								</div>
-								
 							</li>
 					);
 				})}
